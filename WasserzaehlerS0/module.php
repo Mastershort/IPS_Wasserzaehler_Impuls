e175180fa43c594d1346939280977b24fe01d251
Mastershort <BeckerTorsten82@googlemail.com>
2022-01-11 21:59:05 +0100

    fix

--- WasserzaehlerS0/module.php
+++ WasserzaehlerS0/module.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
 		{
 			//Never delete this line!
 			parent::Create();
-			$this->ConnectParent('{6179ED6A-FC31-413C-BB8E-1204150CF376}');
+			$this->ConnectParent('{B36E5D2F-A9BC-BFDB-34D0-0D781B0CE677}');
 			$this->RegisterPropertyInteger('pulseVariableID', 0);
 
 			$this->RegisterPropertyBoolean('Active', false);
