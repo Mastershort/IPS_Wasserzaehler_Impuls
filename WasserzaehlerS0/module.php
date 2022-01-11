24129bf600472ac474b4a657e038913afb5f0e74
Mastershort <BeckerTorsten82@googlemail.com>
2022-01-11 18:50:12 +0100

    First Try

--- WasserzaehlerS0/module.php
+++ WasserzaehlerS0/module.php
@@ -7,8 +7,23 @@ declare(strict_types=1);
 		{
 			//Never delete this line!
 			parent::Create();
-
 			$this->RequireParent('{6179ED6A-FC31-413C-BB8E-1204150CF376}');
+			$this->RegisterPropertyInteger('pulseVariableID', 0);
+
+			$this->RegisterPropertyBoolean('Active', false);
+            $this->RegisterPropertyBoolean('Daily', false);
+            $this->RegisterPropertyBoolean('PreviousDay', false);
+            $this->RegisterPropertyBoolean('PreviousWeek', false);
+            $this->RegisterPropertyBoolean('CurrentMonth', false);
+            $this->RegisterPropertyBoolean('LastMonth', false);
+
+			$this->RegisterPropertyBoolean('Impulse_lBool', false);
+            $this->RegisterPropertyInteger('Impulse_l', 1000);
+			this->RegisterPropertyInteger('UpdateInterval', 600);
+			$this->RegisterTimer('WZ_UpdateCalculation', 0, 'WZ_updateCalculation($_IPS[\'TARGET\']);');
+
+            $this->SetBuffer('Periods', '{}');
+		
 		}
 
 		public function Destroy()
@@ -21,7 +36,23 @@ declare(strict_types=1);
 		{
 			//Never delete this line!
 			parent::ApplyChanges();
-		}
+			
+            $this->MaintainVariable('TodayConsumption', $this->Translate('Daily Consumption'), 2, '~Water', 4, $this->ReadPropertyBoolean('Daily') == true);
+
+            $this->MaintainVariable('PreviousDayConsumption', $this->Translate('Previous Day Consumption'), 2, '~Water', 6, $this->ReadPropertyBoolean('PreviousDay') == true);
+
+            $this->MaintainVariable('PreviousWeekConsumption', $this->Translate('Previous Week Consumption'), 2, '~Water', 8, $this->ReadPropertyBoolean('PreviousWeek') == true);
+
+            $this->MaintainVariable('CurrentMonthConsumption', $this->Translate('Previous Month Consumption'), 2, '~Water', 10, $this->ReadPropertyBoolean('CurrentMonth') == true);
+
+            $this->MaintainVariable('LastMonthConsumption', $this->Translate('Last Month Consumption'), 2, '~Water', 12, $this->ReadPropertyBoolean('LastMonth') == true);
+			
+			$variableIdents = [];
+
+            $variablePosition = 50;
+			$periodsList = $this->getPeriods();
+            $this->SetBuffer('Periods', json_encode($periodsList));
+			}
 
 		public function Send(string $Text)
 		{
